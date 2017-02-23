1f01c95c10fafa619fae07c342ba86329cdd5333
chkrr00k <trknnst132ff@gmail.com>
2017-02-23 22:55:45 +0100

    Update setup.sh

--- production/setup.sh
+++ production/setup.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-if [[ $(id -u) -ne 0]]; then
-	echo "Please insert SUDO password when needed."
-	echo "Installing python libs"
-	sudo apt-get install python-mysqldb
-	sudo apt-get install python-dev
-	sudo apt-get install libmysqlclient-dev
-	echo "Installing MySQL"
-	sudo apt-get install mysql-server mysql-client
-	
-else
-	echo "Installing python libs"
-	apt-get install python-mysqldb
-	apt-get install python-dev
-	apt-get install libmysqlclient-dev
-	echo "Installing MySQL"
-	apt-get install mysql-server mysql-client
-fi
+apt-get install python-mysqldb
+apt-get install python-dev
+apt-get install libmysqlclient-dev
+apt-get install mysql-server mysql-client
 
 
 
